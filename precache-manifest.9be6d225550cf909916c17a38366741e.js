9c8dee11bf3f7e1a4a44a56f1609b7053b4dcfe3
Masud Rana <masudrana2779@gmail.com>
2020-10-28 09:45:51 +0600

    Updates

---
+++ precache-manifest.9be6d225550cf909916c17a38366741e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b72afa03b5e2bcbde76bfce4e8f84c0",
+    "url": "/test-react/index.html"
+  },
+  {
+    "revision": "03855f3055aa09e21425",
+    "url": "/test-react/static/css/main.d4a395de.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/test-react/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/test-react/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03855f3055aa09e21425",
+    "url": "/test-react/static/js/main.e0681bf5.chunk.js"
+  },
+  {
+    "revision": "fac7e645fea9d5fb1132",
+    "url": "/test-react/static/js/runtime-main.dbb0caa0.js"
+  }
+]);
